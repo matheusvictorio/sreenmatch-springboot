adb4faade69e9256f7550edfba645e31df771a86
matheusvictorio <matheusvic2016@gmail.com>
2025-04-24 02:07:31 -0300

    Implementation of CommandLineRunner and applying the API consumption

--- src/main/java/com/praticando/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/praticando/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,21 @@
 package com.praticando.screenmatch;
 
+import com.praticando.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+	}
 }
