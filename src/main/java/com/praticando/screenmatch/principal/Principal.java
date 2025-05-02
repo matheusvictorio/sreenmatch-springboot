88090e8cdfd7eee8d04b453098571e43c6cd9bb2
matheusvictorio <matheusvic2016@gmail.com>
2025-05-01 23:59:34 -0300

    Test

--- src/main/java/com/praticando/screenmatch/principal/Principal.java
+++ src/main/java/com/praticando/screenmatch/principal/Principal.java
@@ -38,8 +38,10 @@ public class Principal {
                 1 - Buscar séries
                 2 - Buscar episódios
                 3 - Listar séries buscadas
+                4 - Buscar série por titulo
+                5 - Buscar sério por ator
                 
-                0 - Sair                                 
+                0 - Sair
                 """;
 
         System.out.println(menu);
@@ -55,6 +57,12 @@ public class Principal {
                 break;
             case 3:
                 listarSeriesBuscadas();
+                break;
+            case 4:
+                buscarSeriePorTitulo();
+                break;
+            case 5:
+                buscarSeriesPorAtor();
                 break;
             case 0:
                 System.out.println("Saindo...");
@@ -86,10 +94,7 @@ public class Principal {
         System.out.println("Digite o nome da série para busca");
         var nomeSerie = sc.nextLine();
 
-        Optional<Serie> serie = series.stream()
-                .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
-                .findFirst();
-
+        Optional<Serie> serie = serieRepository.findByTituloContainingIgnoreCase(nomeSerie);
         if(serie.isPresent()){
             var serieEncontrada = serie.get();
 
@@ -121,5 +126,22 @@ public class Principal {
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
     }
+
+    private void buscarSeriePorTitulo() {
+        System.out.println("Digite o nome da série para busca");
+        var nomeSerie = sc.nextLine();
+
+        Optional<Serie> serieBuscada = serieRepository.findByTituloContainingIgnoreCase(nomeSerie);
+
+        if(serieBuscada.isPresent()){
+
+            System.out.println("Séries encontradas: " + serieBuscada.get());
+
+        } else {
+            System.out.println("Série não encontrada");
+        }
+    }
+
+
 }
 
--- src/main/java/com/praticando/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/praticando/screenmatch/repository/SerieRepository.java
@@ -3,5 +3,9 @@ package com.praticando.screenmatch.repository;
 import com.praticando.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+    // o nome do metodo ja realiza a query
+    Optional<Serie> findByTituloContainingIgnoreCase(String titulo);
 }
